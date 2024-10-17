4b2deac6e4f08428b7862cc44ccac139e61cf35f
avineshsharma <jangraavinesh@gmail.com>
2024-10-17 23:06:34 +0530

    add files

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ alert ("hello avinesh")
 
 console.log ("this is my script")
 console.log ("hello")
+
+
+let userName =prompt("who's there ");
+
+if (userName=="Admin") {
+    console.log("password");
+
+    let pwd=prompt("password");
+
+    if(pwd=="The master"){
+       console.log("Welcome") 
+    }
+    else if(pwd =="cancel"){
+        console.log("canceled")
+    }
+    else{
+       console.log('wrong password')
+    }
+}
+else if (userName=="cancel"){
+    console.log("canceled")
+}
+else {
+    console.log("I don;t know you");
+}
